2c03f48c3568a53c6ba9d4150bf8f9f9303e0453
10974-spec <emmanuelnyakoe13@gmail.com>
2025-07-01 15:46:48 +0300

    first table cell

--- components/CompanionsList.tsx
+++ components/CompanionsList.tsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { cn, getSubjectColor } from "@/lib/utils";
+import Link from "next/link";
 
-const CompanionsList = () => {
-  return (
-    <div>CompanionsList</div>
-  )
+interface CompanionsListProps {
+  title: string;
+  companions?: Companion[];
+  classNames?: string;
 }
 
-export default CompanionsList
+const CompanionsList = ({
+  title,
+  companions,
+  classNames,
+}: CompanionsListProps) => {
+  return (
+    <article className={cn("companion-list", classNames)}>
+      <h2 className="text-3xl font-bold">{title}</h2>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="text-lg w-2/3">Lessons</TableHead>
+            <TableHead className="text-lg">Subject</TableHead>
+            <TableHead className="text-lg text-right">Duration</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {companions?.map(({ id, subject, name, topic, duration }) => (
+            <TableRow key={id}>
+              <TableCell className="font-medium">
+                <Link href={`/companions/${id}`}>
+                  <div className="flex items-center gap-2">
+                    <div className="size-[72px] flex items-center justify-center rounded-lg max-md:hidden" style={{ backgroundColor: getSubjectColor(subject) }}>
+                      <img
+                        src={`/icons/${subject}.svg`}
+                        alt={subject}
+                        width={35}
+                        height={35}
+                      />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <p className="font-bold text-2xl">{name}</p>
+                        <p className="text-gray-500 text-lg">{topic}</p>
+                    </div>
+                  </div>
+                </Link>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </article>
+  );
+};
+
+export default CompanionsList;
