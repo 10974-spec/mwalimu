0972141d5f5243e692ad48e993a40842fe2dce75
10974-spec <emmanuelnyakoe13@gmail.com>
2025-07-01 16:07:05 +0300

    companion list

--- components/CompanionsList.tsx
+++ components/CompanionsList.tsx
@@ -7,6 +7,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { cn, getSubjectColor } from "@/lib/utils";
+import Image from "next/image";
 import Link from "next/link";
 
 interface CompanionsListProps {
@@ -46,12 +47,29 @@ const CompanionsList = ({
                       />
                     </div>
                     <div className="flex flex-col gap-2">
-                        <p className="font-bold text-2xl">{name}</p>
+                        <p className="font-bold text-2xl md-max:truncate">{name}</p>
                         <p className="text-gray-500 text-lg">{topic}</p>
                     </div>
                   </div>
                 </Link>
               </TableCell>
+              <TableCell>
+                <div className="subject-badge w-fit max-md:hidden">
+                    {subject}
+                </div>
+                <div className="flex items-center justify-center rounded-lg w-fit p-2 md:hidden" style={{ backgroundColor: getSubjectColor(subject) }}>
+                   <Image src={`/icons/${subject}.svg`} alt={subject} width={18} height={18} />
+                </div>
+              </TableCell>
+              <TableCell className="text-right">
+                <div className="flex items-center gap-2 w-full justify-end">
+                    <p className="text-2xl">{duration} {''} 
+                        <span className="max-md:hidden">mins</span>
+                    </p>
+                    <Image src="/icons/clock.svg" alt="clock" width={14} height={14} className="md:hidden"/>
+                </div>
+              </TableCell>
+
             </TableRow>
           ))}
         </TableBody>
