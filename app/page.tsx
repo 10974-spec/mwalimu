b3c47f642c51ee9697ac19df959f313cd8479ac9
10974-spec <emmanuelnyakoe13@gmail.com>
2025-07-01 12:01:16 +0300

    folder structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div>
+      <h1 className='text-2xl underline'>Welcome to my saas App</h1>
+      <Button>Start your free trial</Button>
+    </div>
   )
 }
 
